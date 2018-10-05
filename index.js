82935abffb736b11e504755c295945f912269d01
Osmond <runningmon94@gmail.com>
2018-10-05 23:53:49 +0700

    init firebase

---
+++ index.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAcJwqvTv_X2y2FFLO-D3ym2P7x86N0h1M",
+    authDomain: "web-awesome.firebaseapp.com",
+    databaseURL: "https://web-awesome.firebaseio.com",
+    projectId: "web-awesome",
+    storageBucket: "",
+    messagingSenderId: "701214145306"
+};
+firebase.initializeApp(config);
